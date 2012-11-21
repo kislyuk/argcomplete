daac720c1b08661ac4e809ece7c35e4e05aa77b2
Andrey Kislyuk <kislyuk@gmail.com>
2012-11-20 18:06:11 -0800

    Change license to Apache

--- setup.py
+++ setup.py
@@ -1,31 +1,17 @@
 # -*- coding: utf-8 -*-
-"""
-argcomplete
-~~~~
-
-Argcomplete provides easy and extensible automatic tab completion of arguments and options for your Python script.
-
-It makes two assumptions:
-
-- You're using bash as your shell
-- You're using argparse to manage your command line options
-
-See https://github.com/kislyuk/argcomplete for more info.
-
-"""
 
 import os
 from setuptools import setup, find_packages
 
 setup(
     name='argcomplete',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/kislyuk/argcomplete',
-    license='GPL',
+    license='Apache Software License',
     author='Andrey Kislyuk',
     author_email='kislyuk@gmail.com',
     description='Bash tab completion for argparse',
-    long_description=__doc__,
+    long_description=open('README.rst').read(),
     packages = find_packages(),
     scripts = ['scripts/register-python-argcomplete'],
     zip_safe=False,
@@ -34,7 +20,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX',
         'Programming Language :: Python',
