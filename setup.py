025f6a39c5d16065c422525784dff6cd561b9fa0
Andrey Kislyuk <kislyuk@gmail.com>
2012-12-23 20:36:03 -0800

    v0.3.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='argcomplete',
-    version='0.3.0',
+    version='0.3.1',
     url='https://github.com/kislyuk/argcomplete',
     license='Apache Software License',
     author='Andrey Kislyuk',
