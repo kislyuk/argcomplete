d1f5474dec80e74222554a02ecbf512107ce8c52
Andrey Kislyuk <kislyuk@gmail.com>
2013-02-04 08:50:49 -0800

    Match code example and docs

--- docs/examples/describe_github_user.py
+++ docs/examples/describe_github_user.py
@@ -4,7 +4,7 @@ import argcomplete, argparse, requests, pprint
 
 def github_org_members(prefix, parsed_args, **kwargs):
     resource = "https://api.github.com/orgs/{org}/members".format(org=parsed_args.organization)
-    return (member['login'] for member in requests.get(resource).json if member['login'].startswith(prefix))
+    return (member['login'] for member in requests.get(resource).json() if member['login'].startswith(prefix))
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--organization", help="GitHub organization")
@@ -13,4 +13,4 @@ parser.add_argument("--member", help="GitHub member").completer = github_org_mem
 argcomplete.autocomplete(parser)
 args = parser.parse_args()
 
-pprint.pprint(requests.get("https://api.github.com/users/" + args.member).json)
+pprint.pprint(requests.get("https://api.github.com/users/" + args.member).json())
