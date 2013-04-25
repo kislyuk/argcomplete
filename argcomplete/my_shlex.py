d8947d4d20b8566eecb12e2a2df19297e734eede
Andrey Kislyuk <kislyuk@gmail.com>
2013-04-25 11:22:12 -0700

    Revert "my_shlex python3 compatibility fixes"
    This reverts commit abb66f3a823df75774a442d4e31c513f1a407250.

--- argcomplete/my_shlex.py
+++ argcomplete/my_shlex.py
@@ -17,12 +17,9 @@ import sys
 from collections import deque
 
 try:
-    from io import StringIO
+    from cStringIO import StringIO
 except ImportError:
-    try:
-        from cStringIO import StringIO
-    except ImportError:
-        from StringIO import StringIO
+    from StringIO import StringIO
 
 __all__ = ["shlex", "split"]
 
@@ -78,8 +75,14 @@ class shlex:
                 if c in self.wordchars:
                     self.wordchars.remove(c)
 
+        if self.debug:
+            print 'shlex: reading from %s, line %d' \
+                  % (self.instream, self.lineno)
+
     def push_token(self, tok):
         "Push a token onto the stack popped by the get_token method"
+        if self.debug >= 1:
+            print "shlex: pushing token " + repr(tok)
         self.pushback.appendleft(tok)
 
     def push_source(self, newstream, newfile=None):
@@ -90,17 +93,27 @@ class shlex:
         self.infile = newfile
         self.instream = newstream
         self.lineno = 1
+        if self.debug:
+            if newfile is not None:
+                print 'shlex: pushing to file %s' % (self.infile,)
+            else:
+                print 'shlex: pushing to stream %s' % (self.instream,)
 
     def pop_source(self):
         "Pop the input source stack."
         self.instream.close()
         (self.infile, self.instream, self.lineno) = self.filestack.popleft()
+        if self.debug:
+            print 'shlex: popping to %s, line %d' \
+                  % (self.instream, self.lineno)
         self.state = ' '
 
     def get_token(self):
         "Get a token from the input stream (or from stack if it's nonempty)"
         if self.pushback:
             tok = self.pushback.popleft()
+            if self.debug >= 1:
+                print "shlex: popping token " + repr(tok)
             return tok
         # No pushback.  Get a token.
         raw = self.read_token()
@@ -120,6 +133,11 @@ class shlex:
                 self.pop_source()
                 raw = self.get_token()
         # Neither inclusion nor EOF
+        if self.debug >= 1:
+            if raw != self.eof:
+                print "shlex: token=" + repr(raw)
+            else:
+                print "shlex: token=EOF"
         return raw
 
     def read_token(self):
@@ -132,6 +150,9 @@ class shlex:
                 nextchar = self.instream.read(1)
             if nextchar == '\n':
                 self.lineno += 1
+            if self.debug >= 3:
+                print "shlex: in state", repr(self.state), \
+                      "I see character:", repr(nextchar)
             if self.state is None:
                 self.token = ''        # past end of file
                 break
@@ -140,6 +161,8 @@ class shlex:
                     self.state = None  # end of file
                     break
                 elif nextchar in self.whitespace:
+                    if self.debug >= 2:
+                        print "shlex: I see whitespace in whitespace state"
                     if self.token or (self.posix and quoted):
                         break   # emit current token
                     else:
@@ -172,8 +195,10 @@ class shlex:
             elif self.state in self.quotes:
                 quoted = True
                 if not nextchar:      # end of file
+                    if self.debug >= 2:
+                        print "shlex: I see EOF in quotes state"
                     # XXX what error should be raised here?
-                    raise ValueError("No closing quotation")
+                    raise ValueError, "No closing quotation"
                 if nextchar == self.state:
                     if not self.posix:
                         self.token += nextchar
@@ -189,8 +214,10 @@ class shlex:
                     self.token += nextchar
             elif self.state in self.escape:
                 if not nextchar:      # end of file
+                    if self.debug >= 2:
+                        print "shlex: I see EOF in escape state"
                     # XXX what error should be raised here?
-                    raise ValueError("No escaped character")
+                    raise ValueError, "No escaped character"
                 # In posix shells, only the quote itself or the escape
                 # character may be escaped within quotes.
                 if (escapedstate in self.quotes and
@@ -203,6 +230,8 @@ class shlex:
                     self.state = None   # end of file
                     break
                 elif nextchar in self.whitespace:
+                    if self.debug >= 2:
+                        print "shlex: I see whitespace in word state"
                     self.state = ' '
                     if self.token or (self.posix and quoted):
                         break   # emit current token
@@ -238,6 +267,8 @@ class shlex:
                         self._pushback_chars.append(nextchar)
                     else:
                         self.pushback.appendleft(nextchar)
+                    if self.debug >= 2:
+                        print "shlex: I see punctuation in word state"
                     self.state = ' '
                     if self.token:
                         break   # emit current token
@@ -247,6 +278,11 @@ class shlex:
         self.token = ''
         if self.posix and not quoted and result == '':
             result = None
+        if self.debug > 1:
+            if result:
+                print "shlex: raw token=" + repr(result)
+            else:
+                print "shlex: raw token=EOF"
         return result
 
     def sourcehook(self, newfile):
@@ -281,3 +317,16 @@ def split(s, comments=False, posix=True):
     if not comments:
         lex.commenters = ''
     return list(lex)
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        lexer = shlex()
+    else:
+        file = sys.argv[1]
+        lexer = shlex(open(file), file)
+    while 1:
+        tt = lexer.get_token()
+        if tt:
+            print "Token: " + repr(tt)
+        else:
+            break
