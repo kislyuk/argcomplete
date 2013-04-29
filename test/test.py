0355e9d4fcb017a5ba0745b4308f8423bfa4ec2e
Andrey Kislyuk <kislyuk@gmail.com>
2013-04-29 08:47:55 -0700

    non-ascii tests (disabled)

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import locale
 import os
 import sys
@@ -27,6 +28,7 @@ class TestArgcomplete(unittest.TestCase):
 
     def run_completer(self, parser, command, point=None):
         with TemporaryFile() as t:
+            #os.environ['COMP_LINE'] = command.encode(locale.getpreferredencoding())
             os.environ['COMP_LINE'] = command
             os.environ['COMP_POINT'] = point if point else str(len(command))
             os.environ['COMP_WORDBREAKS'] = '"\'@><=;|&(:'
@@ -86,6 +88,23 @@ class TestArgcomplete(unittest.TestCase):
             )
 
         for cmd, output in expected_outputs:
+            self.assertEqual(set(self.run_completer(make_parser(), cmd)), set(output))
+
+    @unittest.skip("currently works on either 2 or 3, but not both")
+    def test_non_ascii(self):
+        def make_parser():
+            parser = argparse.ArgumentParser()
+            parser.add_argument('--книга', choices=['Трудно быть богом', 'Парень из преисподней', 'Понедельник начинается в субботу'])
+            return parser
+
+        expected_outputs = (("prog ", ['--книга', '-h', '--help']),
+            ("prog --книга ", ['Трудно быть богом', 'Парень из преисподней', 'Понедельник начинается в субботу']),
+            ("prog --книга П", ['Парень из преисподней', 'Понедельник начинается в субботу']),
+            ("prog --книга Пу", ['']),
+            )
+
+        for cmd, output in expected_outputs:
+            output = [o.decode(locale.getpreferredencoding()) for o in output]
             self.assertEqual(set(self.run_completer(make_parser(), cmd)), set(output))
 
 if __name__ == '__main__':
