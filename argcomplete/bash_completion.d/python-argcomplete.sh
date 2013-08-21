f6c89aa99aa8dcf5972ac612ebffb1b751df21aa
Red Street <thisisnotme@fake.bearketo.qxpm>
2013-08-20 19:09:42 -0700

    Do not add a trailing space to completion lists with a single entry. Instead, use bash's built in mechanism for this which by default does this (as long as '-o nospace' is not specified under the 'complete' command). Tested under 4.2.37.

--- argcomplete/bash_completion.d/python-argcomplete.sh
+++ argcomplete/bash_completion.d/python-argcomplete.sh
@@ -30,4 +30,4 @@ _python_argcomplete_global() {
         type -t _completion_loader | grep -q 'function' && _completion_loader "$@" 
     fi
 }
-complete -o nospace -o default -D -F _python_argcomplete_global
+complete -o default -D -F _python_argcomplete_global
