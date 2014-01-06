8b1b578d204f460a0bb9a18d3c2e4f662861c2df
Andrey Kislyuk <kislyuk@gmail.com>
2014-01-05 22:55:43 -0800

    v0.6.7

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 
 setup(
     name='argcomplete',
-    version='0.6.6',
+    version='0.6.7',
     url='https://github.com/kislyuk/argcomplete',
     license='Apache Software License',
     author='Andrey Kislyuk',
