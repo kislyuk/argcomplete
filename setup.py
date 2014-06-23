15523a21f190a989e2d6c47467ce86c18d681aa9
Hong Minhee <minhee@dahlia.kr>
2014-06-23 15:04:13 +0900

    Avoid u"" literals to support Python 3.2
    unicode literals don't work on Python 3.2

--- setup.py
+++ setup.py
@@ -33,9 +33,13 @@ setup(
         'Operating System :: MacOS :: MacOS X',
         'Operating System :: POSIX',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Development Status :: 5 - Production/Stable',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
