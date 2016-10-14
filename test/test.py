7817c3144a92a750730dedd83b58e49c54986f94
Evan <evanunderscore@gmail.com>
2016-10-15 00:49:57 +1100

    Add test case to verify #20 is fixed

--- test/test.py
+++ test/test.py
@@ -612,6 +612,25 @@ class TestArgcomplete(unittest.TestCase):
         for cmd, output in expected_outputs:
             self.assertEqual(set(self.run_completer(make_parser(), cmd)), set(output))
 
+    def test_mixed_optional_positional(self):
+        def make_parser():
+            parser = ArgumentParser(add_help=False)
+            parser.add_argument("name", choices=["name1", "name2"])
+            group = parser.add_mutually_exclusive_group()
+            group.add_argument("--get", action="store_true")
+            group.add_argument("--set", action="store_true")
+            return parser
+
+        expected_outputs = (
+            ("prog ", ["--get", "--set", "name1", "name2"]),
+            ("prog --", ["--get", "--set"]),
+            ("prog --get ", ["--get", "name1", "name2"]),
+            ("prog --get name1 ", ["--get "])
+        )
+
+        for cmd, output in expected_outputs:
+            self.assertEqual(set(self.run_completer(make_parser(), cmd)), set(output))
+
 
 class TestArgcompleteREPL(unittest.TestCase):
     def setUp(self):
