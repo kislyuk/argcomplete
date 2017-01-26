kislyuk/argcomplete

commit 5bfae2fa6b4e3291ffd9067b79340ae229c7b9a8

Add custom validators If you need to change default validation behaviour(e.g. x not in y)

argcomplete/__init__.py
@@ -101,8 +101,11 @@ def split_line(line, point):
             else:
                 raise ArgcompleteException("unexpected state? TODO")
 
+def default_validator(c, my_word):
+    return c.startswith(my_word)
+
 def autocomplete(argument_parser, always_complete_options=True, exit_method=os._exit, output_stream=None,
-                 exclude=None):
+        exclude=None, validator=default_validator):
     '''
     :param argument_parser: The argument parser to autocomplete on
     :type argument_parser: :class:`argparse.ArgumentParser`
@@ -262,10 +265,20 @@ def autocomplete(argument_parser, always_complete_options=True, exit_method=os._
                             debug("Resetting completions because", active_action, "is unsatisfied")
                             completions = []
                     try:
-                        completions += [c for c in completer(prefix=cword_prefix,
-                                                             parser=parser,
-                                                             action=active_action,
-                                                             parsed_args=parsed_args) if c.startswith(cword_prefix)]
+                        completions += [c for c in completer(
+                            prefix=cword_prefix,
+                            action=active_action,
+                            parsed_args=parsed_args) 
+                            if validator(c, cword_prefix)
+                        ]
+                    except (SyntaxError, NameError):
+                        # Validator has problems...
+                        # Skip validator in list comprehension
+                        completions += [c for c in completer(
+                            prefix=cword_prefix,
+                            action=active_action,
+                            parsed_args=parsed_args)
+                        ]
                     except (AttributeError, TypeError):
                         # If completer is not callable, try the readline completion protocol instead
                         debug("Could not call completer, trying readline protocol instead")

test/test.py
@@ -221,5 +221,46 @@ class TestArgcomplete(unittest.TestCase):
                 output = [o.decode(locale.getpreferredencoding()) for o in output]
             self.assertEqual(set(self.run_completer(make_parser(), cmd)), set(output))
 
+    def test_custom_validator(self):
+        def make_parser():
+            parser = argparse.ArgumentParser()
+            parser.add_argument('var', choices=['bus', 'car'])
+            parser.add_argument('value', choices=['orange', 'apple'])
+            return parser
+
+        expected_outputs = (("prog ", ['-h', '--help']),
+            ("prog bu", ['']),
+            ("prog bus ", ['-h', '--help']),
+            ("prog bus appl", ['']),
+            ("prog bus apple ", ['-h', '--help']),
+            )
+
+        for cmd, output in expected_outputs:
+            self.assertEqual(set(self.run_completer(make_parser(), cmd, validator=lambda x,y: False) ), set(output))
+
+    def test_different_validators(self):
+        def make_parser():
+            parser = argparse.ArgumentParser()
+            parser.add_argument('var', choices=['bus', 'car'])
+            parser.add_argument('value', choices=['orange', 'apple'])
+            return parser
+
+        validators = (
+                lambda x,y: False,
+                lambda x,y: True,
+                lambda x,y: x.startswith(y),
+        )
+
+        expected_outputs = (("prog ", ['-h', '--help'], validators[0]),
+            ("prog ", ['bus', 'car', '-h', '--help'], validators[1]),
+            ("prog bu", ['bus'], validators[1]),
+            ("prog bus ", ['apple', 'orange', '-h', '--help'], validators[1]),
+            ("prog bus appl", ['apple'], validators[2]),
+            ("prog bus cappl", [''], validators[2]),
+            ("prog bus pple ", ['-h', '--help'], validators[2]),
+            )
+
+        for cmd, output, validator in expected_outputs:
+            self.assertEqual(set(self.run_completer(make_parser(), cmd, validator=validator) ), set(output))
 if __name__ == '__main__':
     unittest.main()

commit 105b1f1eb9fa7584e0e28051bf654d7ebbb1f1f3

Fix bug in 5bfae2f introduced in #77. Fixes #200

argcomplete/__init__.py
@@ -404,7 +404,7 @@ class CompletionFinder(object):
             if completer:
                 if callable(completer):
                     completions_from_callable = [c for c in completer(
-                        prefix=cword_prefix, action=active_action, parsed_args=parsed_args)
+                        prefix=cword_prefix, action=active_action, parser=parser, parsed_args=parsed_args)
                         if self.validator(c, cword_prefix)]
 
                     if completions_from_callable: