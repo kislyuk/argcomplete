b8e86bd19ee54eae3bd3ca4b12dba89120f30e32
Evan <evanunderscore@gmail.com>
2017-01-27 16:16:17 +1100

    Redirect all output to debug stream in debug mode

--- argcomplete/bash_completion.d/python-argcomplete.sh
+++ argcomplete/bash_completion.d/python-argcomplete.sh
@@ -12,6 +12,17 @@ __python_argcomplete_expand_tilde_by_ref () {
     fi
 }
 
+# Run something, muting output or redirecting it to the debug stream
+# depending on the value of _ARC_DEBUG.
+__python_argcomplete_run() {
+    if [[ -z "$_ARC_DEBUG" ]]; then
+        "$@" 8>&1 9>&2 1>/dev/null 2>&1
+    else
+        "$@" 8>&1 9>&2 1>&9 2>&1
+    fi
+    return $?
+}
+
 _python_argcomplete_global() {
     local executable=$1
     __python_argcomplete_expand_tilde_by_ref executable
@@ -45,7 +56,7 @@ _python_argcomplete_global() {
             _ARGCOMPLETE_COMP_WORDBREAKS="$COMP_WORDBREAKS" \
             _ARGCOMPLETE=$ARGCOMPLETE \
             _ARGCOMPLETE_SUPPRESS_SPACE=1 \
-            "$executable" "${COMP_WORDS[@]:1:ARGCOMPLETE-1}" 8>&1 9>&2 1>/dev/null 2>&1) )
+            __python_argcomplete_run "$executable" "${COMP_WORDS[@]:1:ARGCOMPLETE-1}") )
         if [[ $? != 0 ]]; then
             unset COMPREPLY
         elif [[ "$COMPREPLY" =~ [=/:]$ ]]; then
