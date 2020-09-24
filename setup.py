ea981cdad75dbc2e451bd7a3ac17374bf90039da
Steve Wills <steve@mouf.net>
2020-09-24 10:11:23 -0400

    importlib-metadata 2.x works

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 install_requires = []
 tests_require = ["coverage", "flake8", "pexpect", "wheel"]
-importlib_backport_requires = ["importlib-metadata >= 0.23, < 2"]
+importlib_backport_requires = ["importlib-metadata >= 0.23"]
 
 setup(
     name='argcomplete',
