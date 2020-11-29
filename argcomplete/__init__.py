1012ae57ef21a888057545e5a7ed41e610e7c938
Oskar Löfgren <oskar.lofgren@aquarobur.com>
2020-11-29 23:39:25 +0100

    Remove extra newline

--- argcomplete/__init__.py
+++ argcomplete/__init__.py
@@ -683,6 +683,5 @@ def warn(*args):
     """
     # Don't be tempted to use `print("\n",..., *args)`,
     # as that will indent **args** by one space character
-
     print(file=debug_stream)
     print(file=debug_stream, *args)
