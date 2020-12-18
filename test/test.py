d4197606e3b342d49ec4a9ed51f2fe89a603b3f0
Andrey Kislyuk <kislyuk@gmail.com>
2020-12-18 15:14:18 -0800

    Disable all console script tests on MacOS

--- test/test.py
+++ test/test.py
@@ -1300,12 +1300,12 @@ class TestBashGlobal(TestBash):
             command += ' a\t'
             self.assertEqual(self.sh.run_command(command), 'arg\r\n')
 
-    @unittest.skipIf(USING_PYTHON2, "Skip test where pip install fails on Python 2")
+    @unittest.skipIf(os.uname()[0] == "Darwin", "Skip test that fails on MacOS")
     def test_console_script_module(self):
         """Test completing a console_script for a module."""
         self._test_console_script()
 
-    @unittest.skipIf(USING_PYTHON2, "Skip test where pip install fails on Python 2")
+    @unittest.skipIf(os.uname()[0] == "Darwin", "Skip test that fails on MacOS")
     def test_console_script_package(self):
         """Test completing a console_script for a package."""
         self._test_console_script(package=True)
