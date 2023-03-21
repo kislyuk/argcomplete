83f7e211dbb75985f12e336f536f7b618538bcdb
Andrey Kislyuk <kislyuk@gmail.com>
2023-03-20 18:29:42 -0700

    v3.0.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ importlib_backport_requires = ["importlib-metadata >= 0.23, < 6"]
 
 setup(
     name="argcomplete",
-    version="3.0.2",
+    version="3.0.3",
     url="https://github.com/kislyuk/argcomplete",
     project_urls={
         "Documentation": "https://kislyuk.github.io/argcomplete",
