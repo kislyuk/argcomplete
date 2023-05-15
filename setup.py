11c7b49f40a7cf5b5b6ff26b05c301d363094dc3
Your Name <you@example.com>
2023-05-15 10:22:48 +0200

    Moved setuptools settings back to setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@
 
 import glob
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
+    version="3.0.8",
     url="https://github.com/kislyuk/argcomplete",
+    packages=find_packages(exclude=["test", "test.*"]),
     scripts=glob.glob("scripts/*"),
-)
+    package_data={"argcomplete": ["bash_completion.d/python-argcomplete", "py.typed"]},
+    zip_safe=False,
+    include_package_data=True,
+    platforms=["MacOS X", "Posix"],
+ )
