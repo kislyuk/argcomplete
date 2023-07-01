77c631625eda958a587edc33f1c135e7ba316a6a
Hang <hanglei@microsoft.com>
2023-07-02 00:34:31 +0800

    Minor fix (#441)

--- argcomplete/shell_integration.py
+++ argcomplete/shell_integration.py
@@ -111,8 +111,7 @@ Register-ArgumentCompleter -Native -CommandName %(executable)s -ScriptBlock {
     Get-Content $completion_file | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
     }
-    rm $completion_file
-    Remove-Item Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+    Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
 }
 """  # noqa: E501
 
