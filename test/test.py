5c014abe3f1e78f49bf959ca366e57ee83fe71fe
Olivier Gayot <olivier.gayot@canonical.com>
2024-08-02 10:19:21 +0200

    Fix ZSH tests when TERM variable is set to linux
    Running the test-suite with TERM=linux causes the ZSH related tests to
    fail. Fixed by hardcoding TERM="" when running the ZSH tests.
    
    Signed-off-by: Olivier Gayot <olivier.gayot@canonical.com>

--- test/test.py
+++ test/test.py
@@ -65,6 +65,7 @@ class ArgcompleteREPLWrapper(REPLWrapper):
 
 def _repl_sh(command, args, non_printable_insert):
     os.environ["PS1"] = "$"
+    os.environ["TERM"] = ""
     child = pexpect.spawn(command, args, echo=False, encoding="utf-8")
     ps1 = PEXPECT_PROMPT[:5] + non_printable_insert + PEXPECT_PROMPT[5:]
     ps2 = PEXPECT_CONTINUATION_PROMPT[:5] + non_printable_insert + PEXPECT_CONTINUATION_PROMPT[5:]
