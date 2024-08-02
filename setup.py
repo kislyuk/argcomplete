4f9b3d6f40c5e602678122e065dddda95c8bd866
Segev Finer <segev@swimm.io>
2024-08-02 12:53:15 +0300

    Switch to project.scripts from the classic scripts
    Split from #474

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ from setuptools import find_packages, setup
 setup(
     url="https://github.com/kislyuk/argcomplete",
     packages=find_packages(exclude=["test", "test.*"]),
-    scripts=glob.glob("scripts/*"),
     package_data={"argcomplete": ["bash_completion.d/_python-argcomplete", "py.typed"]},
     zip_safe=False,
     include_package_data=True,
